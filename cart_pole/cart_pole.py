640ad7fccb9ed35d4c4c0e67ec7ab61f6c2e36f0
ReanGD <reangd@yandex.ru>
2020-09-28 02:35:40 +0300

    refactoring cart_pole

--- cart_pole/cart_pole.py
+++ cart_pole/cart_pole.py
@@ -2,133 +2,135 @@ import gym
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from typing import List, Tuple
 from tensorflow.keras import layers
+from gym.wrappers.time_limit import TimeLimit
 
 
 class CriticModel:
-    def __init__(self, num_actions):
-        num_inputs = 4
+    # https://keras.io/examples/rl/actor_critic_cartpole/
+    def __init__(self, num_inputs: int, num_actions: int):
         num_hidden = 128
 
         inputs = layers.Input(shape=(num_inputs,))
         common = layers.Dense(num_hidden, activation="relu")(inputs)
         action = layers.Dense(num_actions, activation="softmax")(common)
         critic = layers.Dense(1)(common)
+        self._num_actions = num_actions
         self._model = keras.Model(inputs=inputs, outputs=[action, critic])
 
-    def get_model(self):
-        return self._model
+        self._optimizer = keras.optimizers.Adam(learning_rate=0.01)
+        self._huber_loss = keras.losses.Huber()
+        self._eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
+        # self._tape = tf.GradientTape().__enter__()
 
+        self._episode_count = 0
+        self._total_reward: float = 0.0
+        self._episode_reward: float = 0.0
+        self._rewards_history: List[float] = []
+        self._critic_history: List[float] = []
+        self._log_action_history: List[float] = []
 
+    def predict(self, inputs: np.ndarray):
+        inputs = tf.convert_to_tensor(inputs)
+        inputs = tf.expand_dims(inputs, 0)
+        actions, critic_value = self._model(inputs)
+        action_num = np.random.choice(self._num_actions, p=np.squeeze(actions))
 
-# Configuration parameters for the whole setup
-seed = 42
-gamma = 0.99  # Discount factor for past rewards
-max_steps_per_episode = 10000
-env = gym.make("CartPole-v0")  # Create the environment
-env.seed(seed)
-eps = np.finfo(np.float32).eps.item()  # Smallest number such that 1.0 + eps != 1.0
+        self._critic_history.append(critic_value[0, 0])
+        self._log_action_history.append(tf.math.log(actions[0, action_num]))
 
+        return action_num
 
-num_actions = 2
+    def set_reward(self, value: float):
+        self._rewards_history.append(value)
+        self._episode_reward += value
 
-optimizer = keras.optimizers.Adam(learning_rate=0.01)
-huber_loss = keras.losses.Huber()
-action_probs_history = []
-critic_value_history = []
-rewards_history = []
-running_reward = 0
-episode_count = 0
+    def end_episode(self, tape) -> Tuple[int, float]:
+        self._episode_count += 1;
+        self._total_reward = 0.05 * self._episode_reward + (1 - 0.05) * self._total_reward
 
-cm = CriticModel(num_actions)
-model = cm.get_model()
-is_render = False
-while True:  # Run until solved
-    state = env.reset()
-    episode_reward = 0
-    with tf.GradientTape() as tape:
-        for timestep in range(1, max_steps_per_episode):
-            if is_render:
-                env.render()
-            # env.render(); Adding this line would show the attempts
-            # of the agent in a pop up window.
-
-            state = tf.convert_to_tensor(state)
-            state = tf.expand_dims(state, 0)
-
-            # Predict action probabilities and estimated future rewards
-            # from environment state
-            action_probs, critic_value = model(state)
-            critic_value_history.append(critic_value[0, 0])
-
-            # Sample action from action probability distribution
-            action = np.random.choice(num_actions, p=np.squeeze(action_probs))
-            action_probs_history.append(tf.math.log(action_probs[0, action]))
-
-            # Apply the sampled action in our environment
-            state, reward, done, _ = env.step(action)
-            rewards_history.append(reward)
-            episode_reward += reward
-
-            if done:
-                break
-
-        # Update running reward to check condition for solving
-        running_reward = 0.05 * episode_reward + (1 - 0.05) * running_reward
-
-        # Calculate expected value from rewards
-        # - At each timestep what was the total reward received after that timestep
-        # - Rewards in the past are discounted by multiplying them with gamma
-        # - These are the labels for our critic
-        returns = []
-        discounted_sum = 0
-        for r in rewards_history[::-1]:
+        gamma = 0.99  # Discount factor for past rewards
+        sum_rewards: List[float] = []
+        discounted_sum = 0.0
+        for r in self._rewards_history[::-1]:
             discounted_sum = r + gamma * discounted_sum
-            returns.insert(0, discounted_sum)
+            sum_rewards.insert(0, discounted_sum)
 
         # Normalize
-        returns = np.array(returns)
-        returns = (returns - np.mean(returns)) / (np.std(returns) + eps)
-        returns = returns.tolist()
+        sum_rewards = np.array(sum_rewards)
+        sum_rewards = ((sum_rewards - np.mean(sum_rewards)) / (np.std(sum_rewards) + self._eps)).tolist()
 
-        # Calculating loss values to update our network
-        history = zip(action_probs_history, critic_value_history, returns)
+        # Calculating loss values to update network
         actor_losses = []
         critic_losses = []
-        for log_prob, value, ret in history:
+        history = zip(self._log_action_history, self._critic_history, sum_rewards)
+        for log_action, critic_value, sum_reward in history:
             # At this point in history, the critic estimated that we would get a
             # total reward = `value` in the future. We took an action with log probability
-            # of `log_prob` and ended up recieving a total reward = `ret`.
+            # of `log_action` and ended up recieving a total reward = `sum_reward`.
             # The actor must be updated so that it predicts an action that leads to
             # high rewards (compared to critic's estimate) with high probability.
-            diff = ret - value
-            actor_losses.append(-log_prob * diff)  # actor loss
+            diff = sum_reward - critic_value
+            actor_losses.append(-log_action * diff)
 
-            # The critic must be updated so that it predicts a better estimate of
-            # the future rewards.
+            # The critic must be updated so that it predicts a better estimate of the future rewards.
             critic_losses.append(
-                huber_loss(tf.expand_dims(value, 0), tf.expand_dims(ret, 0))
+                self._huber_loss(tf.expand_dims(critic_value, 0), tf.expand_dims(sum_reward, 0))
             )
 
         # Backpropagation
         loss_value = sum(actor_losses) + sum(critic_losses)
-        grads = tape.gradient(loss_value, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
+        grads = tape.gradient(loss_value, self._model.trainable_variables)
+        self._optimizer.apply_gradients(zip(grads, self._model.trainable_variables))
 
-        # Clear the loss and reward history
-        action_probs_history.clear()
-        critic_value_history.clear()
-        rewards_history.clear()
+        self._episode_reward = 0.0
+        self._rewards_history.clear()
+        self._critic_history.clear()
+        self._log_action_history.clear()
 
-    # Log details
-    episode_count += 1
+        return (self._episode_count, self._total_reward)
+
+
+def model_creator(env: TimeLimit):
+    action_space: gym.spaces.discrete.Discrete = env.action_space
+    observation_space: gym.spaces.box.Box = env.observation_space
+
+    return CriticModel(observation_space.shape[0], action_space.n)
+
+
+env: gym.wrappers.time_limit.TimeLimit = gym.make("CartPole-v0")
+action_space: gym.spaces.discrete.Discrete = env.action_space
+observation_space: gym.spaces.box.Box = env.observation_space
+env.seed(42)
+
+model = CriticModel(observation_space.shape[0], action_space.n)
+
+while True:
+    with tf.GradientTape() as tape:
+        state = env.reset()
+        for _ in range(10000):
+            action_num = model.predict(state)
+            state, reward, done, _ = env.step(action_num)
+            model.set_reward(reward)
+
+            if done:
+                break
+
+        episode_count, total_reward = model.end_episode(tape)
+
     if episode_count % 10 == 0:
         template = "running reward: {:.2f} at episode {}"
-        print(template.format(running_reward, episode_count))
+        print(template.format(total_reward, episode_count))
 
-    if is_render:
-        break
-
-    if running_reward > 155:  # Condition to consider the task solved
+    if total_reward > 195:
         print("Solved at episode {}!".format(episode_count))
-        is_render = True
+        state = env.reset()
+        for _ in range(10000):
+            env.render()
+            action_num = model.predict(state)
+            state, reward, done, _ = env.step(action_num)
+            if done:
+                break
+
+        break
